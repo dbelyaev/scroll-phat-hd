3e669f5c1f5b91cad34e9a68f8f5dfc0f2d4e2fe
Dmitriy Belyaev <me@dmitriy.io>
2018-12-04 22:16:52 +0100

    add simple infite loop in scrolling text script

---
+++ examples/scrolling-simple.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import time
+
+import scrollphathd
+
+print("""
+Scroll pHAT HD: Simple Scrolling
+
+A simple example showing a basic scrolling loop for scrolling
+single messages across the display.
+
+Press Ctrl+C to exit.
+""")
+
+# using in Pimoroni Scroll Bot, need to rotate in order to fix upside down
+scrollphathd.rotate(degrees=180)
+
+while True:
+  scroll_message("HELLO")
+
+
+def scroll_message(message):
+    scrollphathd.clear() # Clear the display and reset scrolling to (0, 0)
+    length = scrollphathd.write_string(message) # Write out your message
+    scrollphathd.show() # Show the result
+    time.sleep(0.5) # Initial delay before scrolling
+
+    length -= scrollphathd.width
+    
+    # Now for the scrolling loop...
+    while length > 0:
+        scrollphathd.scroll(1) # Scroll the buffer one place to the left
+        scrollphathd.show() # Show the result
+        length -= 1
+        time.sleep(0.02) # Delay for each scrolling step
+
+    time.sleep(0.5) # Delay at the end of scrolling
